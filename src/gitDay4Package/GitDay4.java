c641bcd0e613f99f0c10c8b35d26ebf432f59aba
bilalsaglam <62162011+bilalsaglam@users.noreply.github.com>
2020-03-16 20:35:15 -0400

    from aga

--- src/gitDay4Package/GitDay4.java
+++ src/gitDay4Package/GitDay4.java
@@ -5,6 +5,9 @@ public class GitDay4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("local first commit master branch");
+		System.out.println("local branch first commit");
+		System.out.println("someone else contrubuted to the code");
 	}
 
 }
